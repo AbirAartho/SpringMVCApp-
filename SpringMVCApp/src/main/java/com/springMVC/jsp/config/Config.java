d4ef3f51e911a8e7a5b4047b7d9f62040f620653
AbirAartho <151622713+AbirAartho@users.noreply.github.com>
2024-01-25 14:23:27 +0600

    first commit

---
+++ SpringMVCApp/src/main/java/com/springMVC/jsp/config/Config.java
@@ -0,0 +1,23 @@
+package com.springMVC.jsp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+
+
+@Configuration
+public class Config implements WebMvcConfigurer {
+	
+		
+		@Override
+		public void addResourceHandlers(ResourceHandlerRegistry registry) {
+			registry
+			.addResourceHandler("/*.css")
+			.addResourceLocations("/WEB-INF/assets/");
+//			.addResourceLocations("/WEB-INF/assets/");
+		}
+		
+	}
+
